f5652f3d00f67315ff91f167b27de693924d7407
eli <elilevi@gmail.com>
2017-01-07 21:54:03 +0200

    message

--- src/main/java/com/movie/Controller.java
+++ src/main/java/com/movie/Controller.java
@@ -5,12 +5,43 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.sql.Types;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 @RestController
 public class Controller {
 
-    @RequestMapping("/help")
+	@Autowired
+    JdbcTemplate jdbcTemplate;
+	
+    @RequestMapping("/inserttest")
     public String index() {
-        return "Greetings from Movie System";
+
+       
+        //jdbcTemplate.execute("CREATE TABLE customers(" +
+                //"id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+    	
+    	
+    
+    	
+    	String inserQuery = "INSERT INTO users (user_name, first_name, last_name,credits) VALUES (?, ?, ?,?) ";
+		Object[] params = new Object[] { "anniedadon","annie", "dadon",0};
+		int[] types = new int[] { Types.VARCHAR, Types.VARCHAR, Types.VARCHAR ,Types.INTEGER};
+
+		 jdbcTemplate.update(inserQuery, params, types);
+		
+		 String statment= "SELECT * FROM users";
+		 System.out.println( ""+jdbcTemplate.queryForList(statment));
+		 
+		
+		 
+		 
+    	
+        return "table inserted";
+        
+        
+        
     }
 //function getMovie()
 //    return array of array {Id of movie,name,png url,year,rating}
