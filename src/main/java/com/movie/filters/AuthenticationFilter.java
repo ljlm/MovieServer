37a5c7f469005cdaecc3e4db61aade3f4c1c4155
lionelm <lionelm@perfectomobile.com>
2017-03-25 21:21:53 +0300

    asa

--- src/main/java/com/movie/filters/AuthenticationFilter.java
+++ src/main/java/com/movie/filters/AuthenticationFilter.java
@@ -4,6 +4,8 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Base64;
+
 
 /**
  * Created by lionelm on 3/15/2017.
@@ -19,7 +21,7 @@ public class AuthenticationFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpResettableServletRequest wrappedRequest = new HttpResettableServletRequest((HttpServletRequest) servletRequest);
         System.out.println(wrappedRequest);
-
+        byte[] userpass = Base64.getDecoder().decode(wrappedRequest.getHeader("authorization").split(" ")[1].getBytes());
         HttpSession session = ((HttpServletRequest) servletRequest).getSession(true);
         session.setAttribute("authenticated","true");
     }
