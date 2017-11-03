52bdcdbf61c7a6c47f1d792e55b11ea8728edf8f
Lionel <lionel@Lionels-MacBook-Pro.local>
2017-11-03 17:20:02 +0200

    fix for getUserById

--- src/main/java/com/movie/controllers/UserController.java
+++ src/main/java/com/movie/controllers/UserController.java
@@ -1,6 +1,8 @@
 package com.movie.controllers;
 
 import com.movie.services.DataManager;
+import com.movie.tools.ActiveUser;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+import javax.servlet.ServletRequest;
+
 /**
  * Created by lionelm on 6/28/2017.
  */
@@ -17,8 +21,9 @@ import java.util.Map;
 @RestController
 public class UserController {
 
-    @RequestMapping(value = "/users/{userId}", method = RequestMethod.GET)
-    public Map<String, Object> getUserById(@PathVariable Integer userId){
-        return DataManager.getUserDataManager().getUserById(userId);
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public Map<String, Object> getUserById( ServletRequest servletRequest){
+        return DataManager.getUserDataManager().getUserById(ActiveUser.getActiveUserData(servletRequest).getUserId());
     }
+
 }
