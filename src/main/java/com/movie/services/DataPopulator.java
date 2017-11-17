3393ff3e1954330d6b4ca1a36c393633c3a62500
Lionel <lionel@lionels-mbp.mynet>
2017-11-17 23:23:37 +0200

    adding link to movies

--- src/main/java/com/movie/services/DataPopulator.java
+++ src/main/java/com/movie/services/DataPopulator.java
@@ -106,39 +106,48 @@ public class DataPopulator {
 
         dbManager.executeQuery("drop table if exists  movies");
         dbManager.executeQuery("CREATE TABLE movies(" +
-                "id int NOT NULL AUTO_INCREMENT ,movie_name VARCHAR(255) , year int, category int, info VARCHAR(1027) , rating FLOAT ,raters int,available int,locked int,PRIMARY KEY (id))");
+                "id int NOT NULL AUTO_INCREMENT ,movie_name VARCHAR(255) ,pic_link VARCHAR(255) , year int, category int, info VARCHAR(1027) , rating FLOAT ,raters int,available int,locked int,PRIMARY KEY (id))");
         types = new int[] { Types.VARCHAR,Types.INTEGER, Types.INTEGER,Types.VARCHAR , Types.FLOAT ,Types.INTEGER,Types.INTEGER,Types.INTEGER};
-        inserQuery = "INSERT INTO movies (movie_name, year, category, info,rating ,raters ,available,locked ) VALUES (?, ?, ?, ?,?,?,?,?) ";
-        params = new Object[] { "City of God",2002, Categories.CRIME,"Two boys growing up in a violent neighborhood" +
+        inserQuery = "INSERT INTO movies (movie_name, pic_link , year, category, info,rating ,raters ,available,locked ) VALUES (?, ?, ?, ?,?,?,?,?) ";
+
+        params = new Object[] { "City of God","https://images-na.ssl-images-amazon.com/images/M/MV5BNTM4MjZjNWEtMmQxMi00YzY5LTg4ZTAtODJlMDVkZWZmNTVhXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SY1000_CR0,0,677,1000_AL_.jpg",
+                2002, Categories.CRIME,"Two boys growing up in a violent neighborhood" +
                 "of Rio de Janeiro take different paths: one becomes a photographer, the other a drug dealer.",0,0,3,0};
         dbManager.insertQuery(inserQuery, params, types);
 
-        params = new Object[] { "Requiem for a Dream",2000,Categories.DRAMA,"he drug-induced utopias of four Coney Island people are" +
+        params = new Object[] { "Requiem for a Dream","https://images-na.ssl-images-amazon.com/images/M/MV5BOTdiNzJlOWUtNWMwNS00NmFlLWI0YTEtZmI3YjIzZWUyY2Y3XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,666,1000_AL_.jpg"
+                ,2000,Categories.DRAMA,"he drug-induced utopias of four Coney Island people are" +
                 " shattered when their addictions run deep.",0,0,3,0};
         dbManager.insertQuery(inserQuery, params, types);
 
-        params = new Object[] { "The Big Lebowski",1998, Categories.ADVENTURE,"\"The Dude\" Lebowski, mistaken for a millionaire Lebowski," +
+        params = new Object[] { "The Big Lebowski","https://images-na.ssl-images-amazon.com/images/M/MV5BZTFjMjBiYzItNzU5YS00MjdiLWJkOTktNDQ3MTE3ZjY2YTY5XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SY1000_CR0,0,665,1000_AL_.jpg"
+                ,1998, Categories.ADVENTURE,"\"The Dude\" Lebowski, mistaken for a millionaire Lebowski," +
                 " seeks restitution for his ruined rug and enlists his bowling buddies to help get it.",0,0,3,0};
         dbManager.insertQuery(inserQuery, params, types);
 
-        params = new Object[] { "Eternal Sunshine of the Spotless Mind",2004, Categories.DRAMA,"When their relationship turns sour, a couple undergoes" +
+        params = new Object[] { "Eternal Sunshine of the Spotless Mind","https://images-na.ssl-images-amazon.com/images/M/MV5BMTY4NzcwODg3Nl5BMl5BanBnXkFtZTcwNTEwOTMyMw@@._V1_SY1000_CR0,0,674,1000_AL_.jpg"
+                ,2004, Categories.DRAMA,"When their relationship turns sour, a couple undergoes" +
                 " a procedure to have each other erased from their memories. But it is only through the process of loss that they discover what they had " +
                 "to begin with.",0,0,3,0};
         dbManager.insertQuery(inserQuery, params, types);
 
-        params = new Object[] { "Spirited Away",2004, Categories.ANIMATION,"During her family's move to the suburbs, a sullen 10-year-old girl wanders " +
+        params = new Object[] { "Spirited Away","https://images-na.ssl-images-amazon.com/images/M/MV5BOGJjNzZmMmUtMjljNC00ZjU5LWJiODQtZmEzZTU0MjBlNzgxL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SY1000_CR0,0,675,1000_AL_.jpg"
+                ,2004, Categories.ANIMATION,"During her family's move to the suburbs, a sullen 10-year-old girl wanders " +
                 "into a world ruled by gods, witches, and spirits, and where humans are changed into beasts.",0,0,3,0};
         dbManager.insertQuery(inserQuery, params, types);
 
-        params = new Object[] { "Donnie Darko",2001, Categories.DRAMA,"A troubled teenager is plagued by visions of a man in a large rabbit suit who " +
+        params = new Object[] { "Donnie Darko","https://images-na.ssl-images-amazon.com/images/M/MV5BZjZlZDlkYTktMmU1My00ZDBiLWFlNjEtYTBhNjVhOTM4ZjJjXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
+                ,2001, Categories.DRAMA,"A troubled teenager is plagued by visions of a man in a large rabbit suit who " +
                 "manipulates him to commit a series of crimes, after he narrowly escapes a bizarre accident.",0,0,3,0};
         dbManager.insertQuery(inserQuery, params, types);
 
-        params = new Object[] { "Memento",2000, Categories.THRILLER,"A man juggles searching for his wife's murderer and keeping his short-term memory" +
+        params = new Object[] { "Memento","https://images-na.ssl-images-amazon.com/images/M/MV5BZTcyNjk1MjgtOWI3Mi00YzQwLWI5MTktMzY4ZmI2NDAyNzYzXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,681,1000_AL_.jpg"
+                ,2000, Categories.THRILLER,"A man juggles searching for his wife's murderer and keeping his short-term memory" +
                 " loss from being an obstacle.",0,0,3,0};
         dbManager.insertQuery(inserQuery, params, types);
 
-        params = new Object[] { "Catch Me If You Can",2002, Categories.ACCION,"The true story of Frank Abagnale Jr. who, before his 19th birthday, " +
+        params = new Object[] { "Catch Me If You Can","https://images-na.ssl-images-amazon.com/images/M/MV5BMTY5MzYzNjc5NV5BMl5BanBnXkFtZTYwNTUyNTc2._V1_.jpg"
+                ,2002, Categories.ACCION,"The true story of Frank Abagnale Jr. who, before his 19th birthday, " +
                 "successfully conned millions of dollars' worth of checks as a Pan Am pilot, doctor, and legal prosecutor.",0,0,3,0};
         dbManager.insertQuery(inserQuery, params, types);
 
